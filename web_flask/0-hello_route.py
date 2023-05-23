d5b1734588c8633059adb1bdb70ca124581b3814
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-05-23 20:07:25 +0100

    add task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,7 +12,10 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """a method that display hello hbnb"""
+    """
+    Returns:
+        Hello HBNB
+    """
     return "Hello HBNB!"
 
 
